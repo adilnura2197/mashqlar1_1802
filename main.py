60edc148cc890b3e14b27cb3df51374586b76ae7
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-02-18 15:23:03 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+#1
+l = {
+
+}
+print(l)
+
+
+#2
+talaba = {
+    "ism": "Ali",
+    "yosh": 20,
+    "shahar": "Toshkent"
+}
+print(talaba)
+
+
+#3
+talaba = {
+    "ism": "Sardor",
+    "yosh": 22,
+    "shahar": "Samarqand"
+}
+print(talaba["ism"])
+
+
+#4
+talaba = {
+    "ism": "Malika",
+    "yosh": 19,
+    "shahar": "Buxoro"
+}
+print(talaba)
+
+t = {
+    "telefon": "+998901234567"
+}
+talaba.update(t)
+print(f"Talaba kaliti: {talaba}")
+
+
+5
+talaba = {
+    "ism": "Jasur",
+    "yosh": 21,
+    "shahar": "Namangan"
+}
+print(talaba)
+talaba["yosh"] = 23
+print(talaba)
